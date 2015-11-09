9054d6f25863df9417431b14d0a887789f7d5e92
Jordane Barret <jorbarret@me.com>
2015-11-09 19:52:56 +0100

    Projects Backend

---
+++ server/api/project/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var express = require('express');
+var controller = require('./project.controller');
+var auth = require('../../auth/auth.service');
+
+var router = express.Router();
+
+router.get('/', controller.index);
+router.get('/:id', controller.show);
+router.get('/user/:userId', controller.indexId);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.put('/:id', auth.isAuthenticated(), controller.update);
+router.patch('/:id', auth.isAuthenticated(), controller.update);
+router.delete('/:id', auth.isAuthenticated(), controller.destroy);
+
+module.exports = router;
