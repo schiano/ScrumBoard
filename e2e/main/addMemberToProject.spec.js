4e69f6c1b335f2c8db49c9021bdf89dfffce8f2d
Rémi Schiano <remi.schiano@etu.u-bordeaux.com>
2015-11-09 08:55:41 +0100

    Main page object + addMemberToProject test file

---
+++ e2e/main/addMemberToProject.spec.js
@@ -0,0 +1,30 @@
+'use strict';
+
+describe('Add member to project Test Suit', function() {
+	var page;
+
+	beforeEach(function () {
+		browser.get('/');
+		page = require('./main.po');
+	});
+
+	it('should add an existing member to the project', function() {
+
+  	page.clickAddMember();
+  	page.email.sendKeys('test@test.com');
+  	page.addBtn.click();
+
+  	// the test@test user should be present.
+  	//@TODO
+  	});
+
+	it('should fail to add a non-existing member to the project', function() {
+
+  	page.clickAddMember();
+  	page.email.sendKeys('error');
+  	page.addBtn.click();
+
+  	// the error message should be present.
+  	expect(page.errorUnknownUser.isDisplayed()).toBeTruthy();
+  	});
+});
