6c497d9c5237559c08987e607e3e34b930b9037d
ThibautShr <schaerer.thibaut@gmail.com>
2015-11-11 23:46:39 +0100

    Update app.js

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -31,26 +31,29 @@ app.controller('MainCtrl',function($rootScope, $http){
 	$rootScope.check_login = function(){		
 		hide_all_errors();
 		var reg_mail = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-		if(reg_mail.test($rootScope.mail_field_login)){
-			
-			$http.post("../../../server/auth/local/", {
+		if(reg_mail.test($rootScope.mail_field_login)){			
+			$http.post('auth/local', {
 				email: $rootScope.mail_field_login,
 				password: $rootScope.password_field_login
 			}).
 			success(function(data) {
-				alert(data);
-				if(data['id'] != undefined && data['email'] != undefined && data['password'] != undefined && data['role'] != undefined){
-					$rootScope.user = data;
-					sessionStorage.user = JSON.stringify($rootScope.user);
-					next();
+				if(data['token'] != undefined){
+					sessionStorage.token = JSON.stringify(data['token']);
+					$http.get('api/users/me',{ 
+					headers: {'Authorization' : 'Bearer ' + data['token']}}).
+					success(function(data) {
+						sessionStorage.user = JSON.stringify(data);
+						document.location.href='home.html';
+					}).
+					error(function(resultat, statut, erreur){
+							alert(JSON.stringify(resultat,null,4));
+					}.bind(this));
 				}
 				else
 					show_error_mail_login();
 			}).
 			error(function(resultat, statut, erreur){
-					//alert(JSON.stringify(resultat,null,4));
-					alert(statut);
-					//alert(erreur);
+					alert(JSON.stringify(resultat,null,4));
 			}.bind(this));
 		}
 		else
@@ -63,28 +66,30 @@ app.controller('MainCtrl',function($rootScope, $http){
 		   $rootScope.last_name_registration != undefined &&
 		   $rootScope.mail_field_registration != undefined &&
 		   $rootScope.password_field_registration != undefined &&
-		   $rootScope.confirm_password_registration != undefined ){
-			if($rootScope.password_field_registration == $rootScope.confirm_password_field_registration){
+		   $rootScope.confirm_password_field_registration != undefined ){
+			if($rootScope.password_field_registration == $rootScope.confirm_password_field_registration){ 
 				
-				var new_user = { "name" : $rootScope.first_name_registration + " " + $rootScope.last_name_registration,
-								 "email" : $rootScope.mail_field_registration,
-								 "role" : "user",
-								 "hashedPassword" : $rootScope.password_field_registration,
-								 "provider" : "",
-								 "salt" : ""};
+				var new_user = {'_id' : null,
+								'provider': '',
+								'name': $rootScope.first_name_registration + ' ' + $rootScope.last_name_registration,
+								'email': $rootScope.mail_field_registration,
+								'__v': 0,
+								'role': ''};
 				
-				$http.post("../../../server/api/user", new_user).
+				$http.post('api/users/', new_user).
 				success(function(data) {
 					alert('success');
 					if(data['id'] != undefined){
 						$rootScope.user = data;
-						document.location.href="home.html";
+						document.location.href='home.html';
 					}
 					else
 						alert('no make');
 				}).
 				error(function(resultat, statut, erreur){
-						alert(JSON.stringify(resultat,null,4));
+					alert(JSON.stringify(resultat,null,4));
+					alert(statut);
+					alert(erreur);
 				}.bind(this));
 			}
 			else
@@ -102,7 +107,7 @@ app.controller('MainCtrl',function($rootScope, $http){
 		$rootScope.last_name_registration = '';
 		$rootScope.mail_field_registration = '';
 		$rootScope.password_field_registration = '';
-		$rootScope.confirmation_field_registration = '';
+		$rootScope.confirm_password_field_registration = '';
 	}
 
 	//init forms
@@ -117,15 +122,12 @@ app.controller('MainCtrl',function($rootScope, $http){
 app.controller('HomeCtrl', function($scope, $http){	
 	
 	//user connected
-	//$scope.user = JSON.parse(sessionStorage.user);
-	
-	$scope.user = { "name" : "Thibaut",
-					"hashedPassword" : "",
-					"id" : 1};
+	$scope.user = JSON.parse(sessionStorage.user);
+	$scope.token = JSON.parse(sessionStorage.token);
 	
 	// =============== CONTENT PAGE SECTION ===============
 	
-	$scope.content_page = "team.html";
+	$scope.content_page = 'team.html';
 	
 	//switch content
 	$scope.clic_button = function(page){
@@ -144,10 +146,10 @@ app.controller('HomeCtrl', function($scope, $http){
 	//add membre to the current project
 	$scope.addMembre = function(){
 		if($scope.new_membre_name != undefined){
-			var new_member_json = {"name" : $scope.new_membre_name,
-								  "role" : "Developpeur",
-								  "hashedPassword" : "",
-								  "id" : 1 }
+			var new_member_json = {'name' : $scope.new_membre_name,
+								  'role' : 'Developpeur',
+								  'hashedPassword' : '',
+								  'id' : 1 }
 			$scope.projects[$scope.current_project]['users'].push(new_member_json);
 			$scope.initPopupAddMember();
 		}
@@ -162,15 +164,15 @@ app.controller('HomeCtrl', function($scope, $http){
 
 	$scope.addUs = function(){
 		
-		var new_us = {"ID" : 1,
-					  "name" : $scope.new_us_title,
-					  "description" : "Description",
-					  "order" : $scope.new_us_order,
-					  "priority" : $scope.new_us_priority, 
-					  "difficulty" : $scope.new_us_difficulty,
-					  "dependencies" : null };
+		var new_us = {'ID' : 1,
+					  'name' : $scope.new_us_title,
+					  'description' : 'Description',
+					  'order' : $scope.new_us_order,
+					  'priority' : $scope.new_us_priority, 
+					  'difficulty' : $scope.new_us_difficulty,
+					  'dependencies' : null };
 	  
-	  	$http.post("../../../server/api/backlog", new_us).
+	  	$http.post('api/backlog', new_us).
 		success(function(data) {
 			//$scope.current_backlog.push(data);
 		}).
@@ -187,7 +189,7 @@ app.controller('HomeCtrl', function($scope, $http){
 
 	$scope.removeUs = function(index){
 		
-		$http.delete("../../../server/api/backlog", { "id" : index}).
+		$http.delete('api/backlog', { 'id' : index}).
 		success(function(data) {
 			
 		}).
@@ -217,13 +219,23 @@ app.controller('HomeCtrl', function($scope, $http){
 	$scope.current_project = 0;
 	
 	$scope.initProject = function(){
-		//$http.get("../../../server/api/project").
-		$http.get("test_project.json").
+		//$http.get('test_project.json').
+		$http.get('api/projects/user/'+$scope.user['_id']).
 		success(function(data) {
-			$scope.projects = data;
+			if(data != 'null'){
+				$('#dashboard').removeClass('display-none');
+				$('#dashboard-empty').addClass('display-none');
+				$scope.projects = data;
+			}
+			else{
+				$('#dashboard-empty').removeClass('display-none');
+				$('#dashboard').addClass('display-none');
+			}
 		}).
 		error(function(resultat, statut, erreur){
-				alert(JSON.stringify(resultat,null,4));
+			alert(JSON.stringify(resultat,null,4));
+			alert(statut);
+			alert(erreur);	
 		}.bind(this));	
 	}
 	
@@ -231,23 +243,28 @@ app.controller('HomeCtrl', function($scope, $http){
 	
 	$scope.addProject = function(){
 		if($scope.new_project_name != undefined){
-			var new_project = {"name" : $scope.new_project_name,
-							   "state" : 0,
-							   "style" : "progress-bar-success",
-							   "users" : [],
-							   "backlog" : -1
+			var new_project = {'name' : $scope.new_project_name,
+							   'state' : 0,
+							   'style' : 'progress-bar-success',
+							   'users' : [],
+							   'backlog' : -1,
+							   'Path' : ''
 			};
-			
-			$http.post("../../../server/api/project", new_project).
+			$http.post('api/projects', 
+			new_project,
+			{ headers : {'Authorization' : 'Bearer ' + $scope.token }}).
 			success(function(data) {
-
+				alert('success');
+				alert(data);
+				$scope.projects.push(new_project);
+				hide_popup_project();
 			}).
 			error(function(resultat, statut, erreur){
 					alert(JSON.stringify(resultat,null,4));
+					alert(statut);
+					alert(erreur);
 			}.bind(this));
 			
-			$scope.projects.push(new_project);
-			hide_popup_project();
 		}
 		else
 			show_error_name_project();
@@ -256,7 +273,7 @@ app.controller('HomeCtrl', function($scope, $http){
 	$scope.switchProject = function(index){
 		
 		//we fetch the backog
-		$http.post("../../../server/api/backlog", {"id" : $scope.projects[$scope.current_project]['backlog']}).
+		$http.post('../../../server/api/backlog', {'id' : $scope.projects[$scope.current_project]['backlog']}).
 		success(function(data) {
 			$scope.current_backlog = data;
 		}).
