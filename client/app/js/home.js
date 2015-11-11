6c8d70ef505eeabc1d71866d88b066300c40b1e6
ThibautShr <schaerer.thibaut@gmail.com>
2015-11-11 23:48:49 +0100

    Update home.js

--- client/app/js/home.js
+++ client/app/js/home.js
@@ -1,3 +1,4 @@
+'use strict';
 
 //$('#project_0').addClass('background_black_transparent');
 
@@ -51,61 +52,61 @@ hide_all_errors();
 /* POPUP SCRIPT */
 
 function show_popup_project(){
-	$('#sub-body').addClass("sub-body-blur");
+	$('#sub-body').addClass('sub-body-blur');
 	$('#popup-project').removeClass('display-none');
 }
 
 function hide_popup_project(){
 	hide_all_errors();
-	$('#sub-body').removeClass("sub-body-blur");
+	$('#sub-body').removeClass('sub-body-blur');
 	$('#popup-project').addClass('display-none');
 }
 
 function show_popup_team(){
-	$('#sub-body').addClass("sub-body-blur");
+	$('#sub-body').addClass('sub-body-blur');
 	$('#popup-team').removeClass('display-none');
 }
 
 function hide_popup_team(){
 	hide_all_errors();
-	$('#sub-body').removeClass("sub-body-blur");
+	$('#sub-body').removeClass('sub-body-blur');
 	$('#popup-team').addClass('display-none');
 }
 
 function show_popup_us(){
-	$('#sub-body').addClass("sub-body-blur");
+	$('#sub-body').addClass('sub-body-blur');
 	$('#popup-us').removeClass('display-none');
 }
 
 function hide_popup_us(){
 	hide_all_errors();
-	$('#sub-body').removeClass("sub-body-blur");
+	$('#sub-body').removeClass('sub-body-blur');
 	$('#popup-us').addClass('display-none');
 }
 
 /* GRANULARITY CHART */
 
 function makeGranularityChartData(){
-	var scope = angular.element($("body")).scope();
+	var scope = angular.element($('body')).scope();
 
 	var GranularityChartData = {
 		labels : scope.labels,
 		datasets : [
 		{
-			label: "Granularity Chart",
-			fillColor : "rgba(220,220,220,0.2)",
-			strokeColor : "rgba(220,220,220,1)",
-			pointColor : "rgba(220,220,220,1)",
-			pointStrokeColor : "#fff",
-			pointHighlightFill : "#fff",
-			pointHighlightStroke : "rgba(220,220,220,1)",
+			label: 'Granularity Chart',
+			fillColor : 'rgba(220,220,220,0.2)',
+			strokeColor : 'rgba(220,220,220,1)',
+			pointColor : 'rgba(220,220,220,1)',
+			pointStrokeColor : '#fff',
+			pointHighlightFill : '#fff',
+			pointHighlightStroke : 'rgba(220,220,220,1)',
 			data : scope.backlog_difficulty
 		}
 		]
 	}
 
 	//granularity chart
-	var ctxGranularityChart = document.getElementById("canvas-granularity").getContext("2d");
+	var ctxGranularityChart = document.getElementById('canvas-granularity').getContext('2d');
 	window.myLine = new Chart(ctxGranularityChart).Line(GranularityChartData, {
 		responsive: true
 	});
