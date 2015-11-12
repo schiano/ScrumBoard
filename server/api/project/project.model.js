4f9fc0f186905433ae29679da3ca4617e99c4946
Jordane Barret <jorbarret@me.com>
2015-11-12 22:04:25 +0100

    Added fields in project model

--- server/api/project/project.model.js
+++ server/api/project/project.model.js
@@ -4,14 +4,39 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var ProjectSchema = new Schema({
-  name: {type: String, required: true},
-  description: {type: String, required: true},
+  name: {
+	  type: String, required: true
+	  },
+  description: {
+	  type: String, required: true
+	  },
   owner: {
-	  userId: {type: String, required: true},
-	  name: {type: String, required: true}
+	  userId: {
+		  type: String, required: true
+		  },
+	  name: {
+		  type: String, required: true
+		  }
   },
-  members: {type: [{_id:false, userId: {type: String, required: true}, name: {type: String, required: true}, canEdit: {type: Boolean, required: true}}], required: true},
-  isPublic: {type: Boolean, required: true}
+  members: {
+	  type: [{
+		  _id:false, 
+		  userId: {type: String, required: true}, 
+		  name: {type: String, required: true}, 
+		  canEdit: {type: Boolean, required: true}
+		  }], required: true},
+  isPublic: {
+	  type: Boolean, required: true
+	  },
+  backlog: {
+	  type: String
+  },
+  progress: {
+	  type: Number
+  },
+  style: {
+	  type: String
+  }
 });
 
 module.exports = mongoose.model('Project', ProjectSchema);
