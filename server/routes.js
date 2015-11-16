8601f190f28543944e3d663006042f530b30b926
Jordane Barret <jorbarret@me.com>
2015-11-16 14:29:33 +0100

    Added backlog backend

--- server/routes.js
+++ server/routes.js
@@ -13,6 +13,7 @@ module.exports = function(app) {
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
   app.use('/api/projects', require('./api/project'));
+  app.use('/api/backlogs', require('./api/backlog'));
 
   app.use('/auth', require('./auth'));
   
