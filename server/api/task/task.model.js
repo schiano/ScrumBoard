97d211fc9539a77c07fd7b2b0c825debc092a3cc
Jordane Barret <jorbarret@me.com>
2015-11-16 17:41:27 +0100

    Added tasks backend

---
+++ server/api/task/task.model.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var TaskSchema = new Schema({
+  name: {
+	  type: String, required: true
+	  },
+  description: {
+	  type: String, required: true
+	  },
+  order: {
+	  type: Number, required: true
+	  },
+  cost: {
+	  type: Number, required: true
+  	  },
+  dependencies: {
+	  type: [String], required: false
+	  },
+  assigned: {
+	  type: [String], required: false
+	  },
+  progress: {
+	  type: Number, required: true
+  }, 
+  status: {
+	  type: Number, required: true
+  }
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
