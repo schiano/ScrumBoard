ea7f69c9b02e4c439a4e05b9a310d8d8b066dc2e
Jordane Barret <jorbarret@me.com>
2015-11-27 16:29:59 +0100

    Added Project to the dev seed

--- server/config/seed.js
+++ server/config/seed.js
@@ -5,33 +5,12 @@
 
 'use strict';
 
-var Thing = require('../api/thing/thing.model');
 var User = require('../api/user/user.model');
 var Project = require('../api/project/project.model');
 
-Thing.find({}).remove(function() {
-  Thing.create({
-    name : 'Development Tools',
-    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
-  }, {
-    name : 'Server and Client integration',
-    info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
-  }, {
-    name : 'Smart Build System',
-    info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
-  },  {
-    name : 'Modular Structure',
-    info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
-  },  {
-    name : 'Optimized Build',
-    info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
-  },{
-    name : 'Deployment Ready',
-    info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
-  });
-});
 
 User.find({}).remove(function() {
+  // Populating Users
   User.create({
     provider: 'local',
     name: 'Test User',
@@ -43,12 +22,41 @@ User.find({}).remove(function() {
     name: 'Admin',
     email: 'admin@admin.com',
     password: 'admin'
-  }, function() {
+  }, function(err, user1, user2) {
+	  if(err){
+		  console.log('error while populating users');
+		  return;
+	  }
       console.log('finished populating users');
+      // Populating Projects
+      Project.create({
+	      name: "Test Project #1",
+	      description: "This is the first test project, owned by Admin.",
+	      members: [
+		      {
+			      userId: user2._id,
+			      name: "Admin",
+			      canEdit: true
+		      },
+		      {
+			      userId: user1._id,
+			      name: "Test User",
+			      canEdit: true
+		      }
+	      ],
+	      owner: {
+		    userId: user2._id,
+		    name: "Test User"
+		  },
+		  isPublic: true,
+      }, function(err, project){
+	      if(err){
+		  	console.log('error while populating projects');
+		  	return;
+	  	  }
+	      console.log('finished populating projects');
+	      
+      });
     }
   );
-});
-
-Project.find({}).remove(function() {
-	
 });
